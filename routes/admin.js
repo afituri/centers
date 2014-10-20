ed91bc921c6408d108ed21457c5ec9efd5c31541
abdoo ageel <abdooageel@github.com>
2014-10-20 16:02:16 +0200

    I add new root

---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('admin', { title: 'المدرتء' });
+});
+
+router.get('/addmanager', function(req, res) {
+  res.render('addmanager', { title: 'اضافة مدير' });
+});
+
+module.exports = router;
