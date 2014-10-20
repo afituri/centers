245cfbc8ba6a90d4d5d362110b9a92199069043d
abdoo ageel <abdooageel@github.com>
2014-10-20 16:10:45 +0200

    I chenge title

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('admin', { title: 'المدرتء' });
+  res.render('admin', { title: 'المدراء' });
 });
 
 router.get('/addmanager', function(req, res) {
