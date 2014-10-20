1df7f70d836c4feb02c9753c9a0e96991f21aac0
FaisalRahil <faisalrahil@github.com>
2014-10-20 15:26:40 +0200

    adduser

--- routes/root.js
+++ routes/root.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('root', { title: 'المستخدمين' });
+  res.render('', { title: 'root' });
 });
+
+router.get('/adduser', function(req, res) {
+  res.render('adduser', { title: 'adduser' });
+});
+
 module.exports = router;
