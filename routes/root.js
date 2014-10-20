267d635beabc7ead253550fd920639e954e4c1d9
abdoo ageel <abdooageel@github.com>
2014-10-20 15:30:10 +0200

    im Change root

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('', { title: 'root' });
+  res.render('root', { title: 'root' });
 });
 
 router.get('/adduser', function(req, res) {
