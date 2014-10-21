f3f700ccc7844fb48ae75c8d0bbbbd3a68e96198
FaisalRahil <faisalrahil@github.com>
2014-10-21 13:30:40 +0200

    edit validation of textbox

--- public/javascripts/emailvalidat.js
+++ public/javascripts/emailvalidat.js
@@ -1,38 +1,11 @@
 $(document).ready(function(){
-      $('#idshow').hide();
-      $('#ids').change(function(){
-        if($('#ids').val() == '2') {
-          $('#idshow').show(); 
+      $('#constit').hide();
+      $('#level').change(function(){
+        if($('#level').val() == '2') {
+          $('#constit').show(); 
         } else {
-            $('#idshow').hide(); 
+            $('#constit').hide(); 
         }  
     });
-
-
-    $.validator.addMethod("loginRegex", function(value, element) {
-          return this.optional(element) ||/\d/.test(value) && /[a-z]/i.test(value);
-      });
-
-      // validate signup form on keyup and submit
-      $("#form").validate({
-          
-        rules: {
-
-          email: {
-            required: true,
-            minlength: 5,
-            
-          }
-        },
-        messages: {
-          
-          email: {
-            required: "email empty",
-            minlength: "email non",
-            loginRegex: "email Regex."
-          },
-
-        }
-      });
-
 });
+
