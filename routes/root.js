9fb017e85a31962dd81555124818bf03f579c4ce
Anas <bhih.anas@gmail.com>
2014-10-28 16:00:03 +0200

    delet ubdate function

--- routes/root.js
+++ routes/root.js
@@ -15,17 +15,22 @@ router.get('/adduser', function(req, res) {
   res.render('adduser', { title: 'إضافة مستخدم' });
 });
 
+router.get('/editroot/:id', function(req, res) {
+  userMgr.getUser(req.params.id,function(result){
+    res.render('editroot',{title: 'تعديل مستجدم', user : result});
+  })
+});
+
 /* POST adduser form for root */
 router.post('/adduser', function(req, res) {
   userHelpers.addUser(req, function (results){
     userMgr.getUsers(function(results){
-      res.render('root',{title: 'root', users : results});
+      res.render('root',{title: 'المستخدمين', users : results});
     });
   });
 });
 /* get userby id form for root */
 router.get('/getUser/:id', function(req, res) {
-  console.log("got here");
   userMgr.getUser(req.params.id,function(result){
     res.send(result);
   })
