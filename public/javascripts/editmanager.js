e046d0106759f918285f4847f5c35b27c7b31357
FaisalRahil <faisalrahil@github.com>
2014-10-30 11:34:05 +0200

    edit

--- public/javascripts/editmanager.js
+++ public/javascripts/editmanager.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+  /*  */
+  $('#user .editable').editable('disable');
+  $('#enable').click(function() {
+    $('#user .editable').editable('toggleDisabled');
+  }); 
+    
+
   $('#level').editable({
     url: '/users/edit',
     source:[
@@ -41,4 +48,24 @@ $(document).ready(function(){
     name: 'email',
     title: 'Enter email',
   });
+  
+  /*  */
+  $('#name').editable('option', 'validate', function(v) {
+    if(!v) return 'الرجاء ادخال اسم المستخدم';
+  });
+  /*  */
+  $('#phone').editable('option', 'validate', function(v) {
+    var flag = /[0-9]{10}/.test(v);
+    if(!v) return 'الرجاء ادخال رقم الهاتف';
+    if(v.length<10) return "يجب أن يكون الهاتف  لا يقل عن 10 ارقام";
+    if(!flag) return "هذا ليس رقم هاتف";
+  });
+  /*  */
+  $('#email').editable('option', 'validate', function(v) {
+    if(!v) return 'الرجاء ادخال بريد اليكتروني';
+    var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    var valid = emailReg.test(v);
+    if(!valid) return 'هذا ليس بريد اليكتروني';
+  });
+
 });
