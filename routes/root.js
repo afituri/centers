932abce8cc366526dcf87be144403bdfaa5f42db
Anas <bhih.anas@gmail.com>
2014-11-03 14:24:43 +0200

    add root

--- routes/root.js
+++ routes/root.js
@@ -2,33 +2,32 @@ var express = require('express');
 var userHelpers = require('../app/userHelpers');
 var router = express.Router();
 var userMgr = require('../app/user').userMgr;
+var log = require('../app/log').repo;
+
 
 /* GET home page. */
 router.get('/',userHelpers.isRoot, function(req, res) {
   userMgr.getUsers(function(results){
     res.render('root',{title: 'المستخدمين', users : results});
-  })
+  });
 });
 /* Add user page. */
 router.get('/adduser',userHelpers.isRoot, function(req, res) {
   userMgr.getOffice(function(result){
     res.render('adduser', { title: 'إضافة مستخدم' , offices : result  });
-  })
+  });
 });
-<<<<<<< HEAD
 
-router.get('/edituser/:id', function(req, res) {
-=======
 /* Edit user page. */
-router.get('/editroot/:id',userHelpers.isRoot, function(req, res) {
->>>>>>> 5c6dd4de2c038b35d02b057d2129afdcf3b3a243
+router.get('/edituser/:id',userHelpers.isRoot, function(req, res) {
   userMgr.getUser(req.params.id,function(result){
     res.render('edituser',{title: 'تعديل مستجدم', user : result});
-  })
+  });
 });
 /* POST adduser form for root */
 router.post('/adduser', userHelpers.isRoot, function(req, res) {
   userHelpers.addUser(req.body, function (results){
+    log.insertLog(req.session.iduser,"add","user"," add new user name : "+results.name,results.id);
     userMgr.getUsers(function(results){
       res.render('root',{title: 'المستخدمين', users : results});
     });
@@ -38,13 +37,13 @@ router.post('/adduser', userHelpers.isRoot, function(req, res) {
 router.get('/getUser/:id', function(req, res) {
   userMgr.getUser(req.params.id,function(result){
     res.send(result);
-  })
+  });
 });
 /* Delete user page. */
 router.get('/deleteUser/:id', function(req, res) {
   userMgr.delUser(req.params.id,function(result){
     res.send(result);
-  })
+  });
 });
 
 module.exports = router;
