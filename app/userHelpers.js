78a1dd2c842ebc19273c77dde6e55ac0273c1ce7
Ahmed Fituri <ahmed.elfituri@gmail.com>
2014-11-10 16:29:58 +0200

    applying pagination

--- app/userHelpers.js
+++ app/userHelpers.js
@@ -1,5 +1,6 @@
 var generatePassword = require('password-generator'),
   easyPbkdf2 = require("easy-pbkdf2")(),
+  url=require('url'),
   mailer = require('../app/mailer');
   userMgr = require('../app/user').userMgr;
 
@@ -47,5 +48,60 @@ module.exports = {
   isAdmin : function (req,res,next) {
     if (req.isAuthenticated() && req.session.level<=1) { return next(); }
     res.redirect('/users/login')
+  },
+  getPage : function (req){
+    var page = 1;
+    if(url.parse(req.url, true).query.p){
+      page = parseInt(url.parse(req.url, true).query.p);
+    }
+    return page;
+  },
+  getLimit : function (page){
+    var limit =0;
+    if (page > 1)
+      limit = page * 10 - 10;
+    return limit;
+  },
+  getPageCount : function(count){
+    return Math.ceil(count/10);
+  },
+
+  paginate : function(page,pageCount){
+    var pagination={}, pages = [], next={}, previous={};
+    var i = 0, limit = 10, ret ='';
+    //listing pages
+    page =Number(page);
+    pageCount= Number(pageCount);
+    var leftCount = Math.ceil(limit / 2) - 1;
+    var rightCount = limit - leftCount - 1;
+    if (page + rightCount > pageCount)
+      leftCount = limit - (pageCount - page) - 1;
+    if (page - leftCount < 1)
+      leftCount = page - 1;
+    var start = page - leftCount;
+    while (i < limit && i < pageCount) {
+      newContext = { n: start };
+      if (start === page) newContext.active = "active ";
+      pages.push(newContext);
+      start++;
+      i++;
+    }
+    //defining next
+    if (page === 1) {
+      previous = { disabled: "disabled", n: 1 }
+    }
+    else {
+      previous = { n: page - 1 }
+    }
+    //defining next
+    newContext = {};
+    if (page === pageCount) {
+      next = { disabled: "disabled", n: pageCount }
+    }
+    else {
+      next = { n: page + 1 }
+    }
+    pagination = {pages : pages, next : next, previous : previous};
+    return pagination;
   }
 };
