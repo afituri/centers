6ae2c92daad2be0c58859d43e4bdf0b278012968
abdelqudoss <doss.aburass@gmail.com>
2014-12-02 14:07:56 +0200

    add butn viw

--- routes/center.js
+++ routes/center.js
@@ -6,13 +6,15 @@ var employeeMgr = require('../app/employee').employeeMgr;
 var employeess =[];
 router.get('/:cid',userHelpers.isCenter, function(req, res) {
   centerMgr.getCenter(req.params.cid,function(results){
+    req.session.back = req.originalUrl;
+    console.log(req.session.back+"print me");
     var page = userHelpers.getPage(req);
     var limit = userHelpers.getLimit(page);
     employeeMgr.getEmployeeCenter(limit,req.params.cid,function(result){
       if(result[1][0] != undefined){
         var pageCount = userHelpers.getPageCount(result[1][0].cnt); //cnt is the total count of records
         var pagination = userHelpers.paginate(page,pageCount);
-        res.render('center', { title: 'مركز البينات ' , cent : results,employees:result[0], pagination : pagination });
+        res.render('center', { title: 'مركز البينات ' , cent : results,employees:result[0], pagination : pagination , url: req.session.back});
       } else {
         res.render('center', { title: 'مركز البينات ' , cent : results,employees:employeess, pagination : null });
       }
