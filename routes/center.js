33ab34e7d3c75b692a8276bfa2954a58028f4fcf
Joabdooageel <abdooageel@github.com>
2014-12-02 17:57:53 +0200

    pagination

--- routes/center.js
+++ routes/center.js
@@ -7,7 +7,6 @@ var employeess =[];
 router.get('/:cid',userHelpers.isCenter, function(req, res) {
   centerMgr.getCenter(req.params.cid,function(results){
     req.session.back = req.originalUrl;
-    console.log(req.session.back+"print me");
     var page = userHelpers.getPage(req);
     var limit = userHelpers.getLimit(page);
     employeeMgr.getEmployeeCenter(limit,req.params.cid,function(result){
--- routes/report.js
+++ routes/report.js
@@ -2,12 +2,20 @@ var express = require('express');
 var userHelpers = require('../app/userHelpers');
 var router = express.Router();
 var logMgr = require('../app/log').repoMgr;
+var res =[];
 
 /* GET home page. */
 router.get('/', userHelpers.isAdmin,function(req, res) {
-  logMgr.report(function(results){
-    res.render('report',{title: 'التقارير',result:results});
+  var page = userHelpers.getPage(req);
+  var limit = userHelpers.getLimit(page);
+  logMgr.report(limit,function(results){
+    if(results[1][0] != undefined){
+      var pageCount = userHelpers.getPageCount(results[1][0].cnt); //cnt is the total count of records
+      var pagination = userHelpers.paginate(page,pageCount);
+      res.render('report',{title: 'التقارير',result:results[0], pagination : pagination});
+    }else{
+      res.render('report',{title: 'التقارير',result:res, pagination : null});
+    }
   }); 
 });
-
 module.exports = router;
