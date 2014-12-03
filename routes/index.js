cdeca1e3c494f368656bdd934388bfbaa0db3f77
FaisalRahil <faisalrahil@github.com>
2014-12-03 12:25:37 +0200

    edit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var userHelpers = require('../app/userHelpers');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', userHelpers.isAdmin, function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
