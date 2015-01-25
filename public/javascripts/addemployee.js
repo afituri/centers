df13a4d77a2af38708ca0dbbffbc8d773217d085
abdooageel <ageel.abdoo@gmail.com>
2015-01-25 15:01:12 +0200

    fixed

--- public/javascripts/addemployee.js
+++ public/javascripts/addemployee.js
@@ -1,35 +1,54 @@
 $(document).ready(function(){
 	$.get('/employee/getoffice',function(result){
 	$('#office_idoffice').empty();
+	$('#office_idoffice').append("<option value = ''></option>");
 		for ( var i = 0; i < result.length;  i++ ) {
 			$('#office_idoffice').append("<option value = '"+result[i].office_id+"'>"+result[i].office_name_ar+"</option>");
 		}
 	});
 	$('#office_idoffice').click(function() {
+		$('#su').addClass('hidden');
+		$('#ce').addClass('hidden');
+		$('#ty').addClass('hidden');
 		var id = $('#office_idoffice').val();
+		if(id !=''){
+		$('#su').removeClass('hidden');
 		$('#subconstituency_idsubconstituency').empty();
 		$('#center_idcenter').empty();
 		$('#type').empty();
+			$('#subconstituency_idsubconstituency').append("<option value = ''></option>");
+
 		$.get('/employee/getsubconstituency/'+id,function(result){
 			for ( var i = 0; i < result.length;  i++ ) {
 				$('#subconstituency_idsubconstituency').append("<option value = '"+result[i].subconstituency_id+"'>"+result[i].subconstituency_name_ar+"</option>");
 			}
 		});
+	}
 	});
 	$('#subconstituency_idsubconstituency').click(function() {
 		var ido = $('#office_idoffice').val();
 		var ids = $('#subconstituency_idsubconstituency').val();
+		$('#ce').addClass('hidden');
+		$('#ty').addClass('hidden');
 		$('#center_idcenter').empty();
 		$('#type').empty();
+		if( ids!=''){
+			$('#center_idcenter').append("<option value = ''></option>");
 		$.get('/employee/getcenter/'+ido+"/"+ids,function(result){
+			$('#ce').removeClass('hidden');
 			for ( var i = 0; i < result.length;  i++ ) {
 				$('#center_idcenter').append("<option value = '"+result[i].center_id+"'>"+result[i].name+"</option>");
 			}
 		});
+	}
 	});
 	$('#center_idcenter').click(function() {
 		var id = $('#center_idcenter').val();
+		$('#ty').addClass('hidden');
+		if(id !=''){
 		$.getJSON( "employee/employee_type", function( json ) {
+			$('#type').append("<option value = ''></option>");
+			$('#ty').removeClass('hidden');
 				if (id < 0)
 					var type=json.hnec;
 				else 
@@ -38,9 +57,17 @@ $(document).ready(function(){
 			  	$('#type').append("<option value = '"+key+"'>"+type[key]+"</option>");
 			  } 
 		 });
+	}
 	});
 	$("#form").validate({
 		rules: {
+			office_idoffice:{
+				required: true,
+			},
+			type:{
+				required: true,
+
+			},
 			subconstituency_idsubconstituency: {
 				required: true,
 			},
@@ -79,6 +106,12 @@ $(document).ready(function(){
 				remote: "هذا البريد الالكتروني تم تسجيله من قبل الرجاء اختيار بريد آخر"
 			},
 			subconstituency_idsubconstituency: {
+				required: "الرجاء الاختيار ",
+			},
+			type:{
+				required: "الرجاء الاختيار ",
+			},
+			office_idoffice: {
 				required: "الرجاء الاختيار ",
 			},
 			center_idcenter: {
