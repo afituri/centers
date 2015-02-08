b9a6ffa4a01d1ec6287334740d40cf82bd0981d0
abdelqudoss <doss.aburass@gmail.com>
2015-02-08 08:59:49 +0200

    Employee office

---
+++ app/employeeOffice.js
@@ -0,0 +1,65 @@
+var mysqlMgr = require('./mysql').mysqlMgr,
+  util=require('util');
+exports.employeeOfficeMgr = {
+
+  /* get employee  */
+
+  getEmployeeOffice : function(oid,cb){
+    mysqlMgr.connect(function (conn) {
+      console.log(oid)
+      conn.query('SELECT `e`.`id_emp_office`, `e`.`emp_office_name`, `e`.`email`, `e`.`type`, `e`.`nid`, `p`.`phone_number`,`e`.`office_id`, `o`.`office_name_ar`, `e`.`acount_number`, `e`.`bank_name` FROM  `office` o,`employeeOffice` e LEFT JOIN `phone` p ON ( `p`.`user_employee` = `e`.`id_emp_office` AND `p`.`user_type`=2 AND `p`.`status`=1)  WHERE `o`.`office_id`= `e`.`office_id` AND`e`.`office_id`= ? AND`e`.`status` = 1 ' ,[oid],function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        } 
+      });
+    });
+  },
+    addemployeeoffice : function(body,cb){
+    mysqlMgr.connect(function (conn) {
+      var phone = body.phone;
+      var type = body.phone_type;
+      var p_type = body.p_type;
+      delete body["p_type"];
+      delete body["phone"];
+      delete body["phone_type"];
+      conn.query('INSERT INTO `employeeOffice` SET ?',  body,  function(err, result) {
+        if(err) {
+          util.log(err);
+        } else {
+          var results={
+            id:result.insertId,
+            name :body.employee_name
+          }
+          for (var i=0;i<phone.length;i++) {
+              conn.query('INSERT INTO `phone` SET `user_type` = 2, `phone_number` = ?,type=?,`user_employee` =? ,`p_type` =?',[phone[i],type[i],results.id,p_type[i]]);           
+              }
+        conn.release();
+          cb(results); 
+        }
+      });
+    });
+  },
+  /*delete employee */
+  deleteemployeeoffice : function(id,cb){
+    mysqlMgr.connect(function (conn) {
+      var date = new Date();
+      conn.query('UPDATE `employeeOffice` SET `status` = 0 ,`modify_date`= ? WHERE `id_emp_office` = ?',[date,id],  function(err, result) {
+        conn.query('SELECT `idphone`,`phone_number` FROM  `phone` WHERE `status` = 1  AND `user_type`=2 AND `user_employee`=?',id,  function(err, results) {
+          conn.query('SELECT `emp_office_name` FROM  `employeeOffice` WHERE `id_emp_office` = ? ',id,  function(err, resultz) {
+            conn.query('UPDATE `phone` SET `status` = 0 ,`modify_date` = ? WHERE `user_type`= 2 AND`user_employee` = ?',[date,id]);
+            conn.release();
+            if(err) {
+              util.log(err);
+            } else {
+              console.log(results);
+              cb(results,resultz);
+            } 
+          });
+        });
+      });
+    });
+  },
+};
---
+++ public/javascripts/employeeOffice.js
@@ -0,0 +1,130 @@
+$(document).ready(function(){
+ /* Go to emlpoyeeOffice needs view or edit */
+ 
+  // $("[data-toggle=popover]").popover();
+  // $('#idcenter').on('input', function(){
+  //   $.typet=new Array();
+  //   if($('#idcenter').val().length >=3) {
+  //     $.get('/center/searchEmpInCenter/'+$('#idcenter').val()+'/'+$('#deleteemployee').data('center'),function(result){
+  //       $('#centers').empty();
+  //       $('.pagination').hide();
+  //       $.getJSON( "/employee/employee_type", function( json ) {
+  //         for(key in json.hnec){
+  //           $.typet.push(json.hnec[key]);
+  //         }
+  //         for(key in json.employee){
+  //           $.typet.push(json.employee[key]);
+  //         }
+  //         for(key in result){
+  //           var p = "";
+  //           if (result[key].phone_number != null){
+  //           p = result[key].phone_number ;
+  //           }
+  //           $('#centers').append('<tr><td >'+result[key].employee_name+'</td><td><a id="phone" href="#phonee" data-toggle="modal" onClick="pho('+result[key].idemployee+');"data-value="'+result[key].idemployee+
+  //                               '">'+p+'</a></td><td>'+$.typet[result[key].type]+'</td><td width="12%"><a href="#" tabindex="0" id="'+result[key].idemployee+'" data-toggle="popover" data-trigger="hover" data-placement="top" title="اسم المركز" data-content="'+result[key].name+'">'+result[key].name+'</a>'+
+  //                               '</td><td width="3%"><a class="btn btn-primary btn-xs"href="/employee/editemployee/'+result[key].idemployee+'">'+
+  //                               '<span class="glyphicon glyphicon-eye-open"></span></a></td><td width="3%"><a class="btn btn-danger btn-xs"onClick="del('+result[key].idemployee+');" href="#del"data-toggle="modal"> <span class="glyphicon glyphicon-trash"></span></a> </td></tr>');
+  //       }
+  //         $("[data-toggle=popover]").popover();
+  //     });
+  //      });
+  //   }
+  // });
+  // $('body').on('click', '#radioBtn a', function () {
+  //   var sel = $(this).data('title');
+  //   var tog = $(this).data('toggle');
+  //   var a=$(this).siblings("#p_type" );
+  //   a.val(sel);
+  //   $(this).siblings("a").removeClass('active').addClass('notActive');
+  //   $(this).removeClass('notActive').addClass('active');
+  // })
+
+  // $("a[id^='phone']").click(function() {
+  //   var id = $(this).data("value");   
+  //   $.get('/employee/getphone/'+id, function(result){
+  //    $('#body').empty();
+  //     $('#emaill').empty();
+  //     $('#emaill').append("<tr><td><strong>البريد الالكتروني </strong></td><td>"+result[0].email+"</td></tr>");
+  //     for ( var i = 0; i < result.length;  i++ ) {
+  //       $('#body').append("<tr><td>"+result[i].phone_number+"</td><td>"+result[i].type+"</td><td>"+result[i].p_type+"</td></tr>");
+  //     }
+  //   });
+  // });
+  /* Go to employee needs view or edit */
+  $("button[id^='viw']").click(function() {
+    var id = $(this).val();
+    window.location.href="/employee/editemployee/"+id;
+  });
+    $('body').on('click', '#delete', function () {
+    var id = $(this).val();
+    $('#deleteemployee').val(id);
+    $("#deleteemployee").attr('data-id',$(this).data("value"));
+
+  });
+  $('#deleteemployee').click(function() {
+    var id = $(this).val();
+    var center = $(this).data("id");
+    $.get('/office/deleteemployee/'+id, function(result){
+      window.location.href="/office/"+center+"/EmployeeOffice";
+    });
+  });
+  //  $("#form").validate({
+  //   rules: {
+  //     subconstituency_idsubconstituency: {
+  //       required: true,
+  //     },
+  //     center_idcenter: {
+  //       required: true,
+  //     },
+  //     employee_name: {
+  //       required: true,
+  //     },
+  //     email: {
+  //       required: true,
+  //       email: true,
+  //       remote: {
+  //         url :"/employee/checkEmail",
+  //         type : "post",
+  //         data: {
+  //           email: function() {
+  //             return $( "#email" ).val();
+  //           }
+  //         }
+  //       }
+  //     },
+  //     'phone[]': {
+  //       required: true,
+  //       minlength: 10,
+  //       number: true,
+  //     }
+  //   },
+  //   messages: {
+  //     employee_name: {
+  //       required: "الرجاء ادخال اسم المستخدم",
+  //     },
+  //     email: {
+  //       required: " هذا ليس بريد اليكتروني ",
+  //       email: "هذا ليس بريد اليكتروني",
+  //       remote: "هذا البريد الالكتروني تم تسجيله من قبل الرجاء اختيار بريد آخر"
+  //     },
+  //     subconstituency_idsubconstituency: {
+  //       required: "الرجاء الاختيار ",
+  //     },
+  //     center_idcenter: {
+  //       required: "الرجاء الاختيار ",
+  //     },
+  //     'phone[]': {
+  //       required: "الرجاء ادخال رقم الهاتف",
+  //       minlength: " يجب أن يكون الهاتف لا يقل عن 10 ارقام ",
+  //       number: "الرجاء ادخال رقم الهاتف ",
+  //     }
+  //   },
+  //   errorPlacement: function(error, element) {
+  //     if (element.attr("name") == "phone[]") {
+  //       error.insertAfter("#phone_input");
+  //     } else {
+  //       error.insertAfter(element);
+  //     }
+  //   }
+  // });
+});
